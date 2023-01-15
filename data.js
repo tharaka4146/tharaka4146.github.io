802e6c2ace14837cca0428f2aa93397a4e11008c
tharaka4146 <tharaka4146@gmail.com>
2023-01-15 21:32:12 +0530

    changes hehe

---
+++ data.js
@@ -0,0 +1,48 @@
+console.log('sdf')
+
+var result = {
+    "start": {
+        "count": "5",
+        "title": "start",
+        "priorities": [{
+            "txt": "Work"
+        }, {
+            "txt": "Time Sense"
+        }, {
+            "txt": "Dicipline"
+        }, {
+            "txt": "Confidence"
+        }, {
+            "txt": "CrossFunctional"
+        }]
+    }
+}
+
+console.log('result', result)
+
+// for (const id in result) {
+//     console.log(result[id])
+
+//     var one = document.createElement("div");
+//     one.className = "col-lg-2";
+//     one.style.margin = "15px";
+
+//     var two = document.createElement("div");
+//     two.className = "card";
+
+//     var three = document.createElement("iframe");
+//     three.width = "225";
+//     three.height = "400";
+//     three.src = `https://www.youtube.com/embed/${result[id]}`;
+//     three.style.borderRadius = "10px";
+//     three.frame
+//     three.allowFullscreen = "allowfullscreen";
+
+
+//     one.appendChild(two);
+//     two.appendChild(three);
+
+//     console.log('one', one)
+
+//     document.getElementById("tit").appendChild(one);
+// }
