6df1ec22929894c59aaeb2360889dbe19a4a8cff
tharaka4146 <tharaka4146@gmail.com>
2023-01-16 06:04:58 +0530

    sss

--- shorts.js
+++ shorts.js
@@ -26,7 +26,7 @@ console.log(1)
 // keys = Object.keys(localStorage),
 
 
-console.log('ffff');
+console.log('mmmm');
 
 
 // for (var i = 0, len = localStorage.length; i < len; ++i) {
@@ -38,9 +38,10 @@ let idArray = Object.keys(localStorage)
 console.log('idArray', idArray)
 
 for (values in idArray) {
-    console.log('idArray[values]', idArray[values])
-    console.log('Number(idArray[values])', Number(idArray[values]))
-    idIntArray.push(Number(idArray[values]))
+    console.log('values', values)
+    // console.log('idArray[values]', idArray[values])
+    // console.log('Number(idArray[values])', Number(idArray[values]))
+    // idIntArray.push(Number(idArray[values]))
 }
 
 console.log('idIntArray', idIntArray)
